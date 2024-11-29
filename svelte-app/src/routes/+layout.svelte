df9411c24b96850c2cffc07e154e359f92b07210
Emmanuel FITOUSSI <manu.fitoussi@gmail.com>
2024-11-29 23:14:03 +0100

    first commit

---
+++ svelte-app/src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	export const csr = true;
+	export const prerender = false;
+	export const ssr = false;
+
+	import '../app.css';
+	let { children } = $props();
+</script>
+
+{@render children()}

4b5e2f8b10a2cc1ded4d387d978f80bd7b25c5d5
Emmanuel FITOUSSI <manu.fitoussi@gmail.com>
2024-12-02 23:36:45 +0100

    Ajoute la prise en charge de Flowbite et met à jour la mise en page avec des composants de navigation

--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -1,6 +1,37 @@
 <script lang="ts">
 	import '../app.css';
 	let { children } = $props();
+
+	import { Button, Navbar, NavBrand, NavHamburger, NavLi, NavUl } from 'flowbite-svelte';
+	import { CogOutline, HomeOutline } from 'flowbite-svelte-icons';
+	import { page } from '$app/stores';
+	
+
+	let title = $state('');
+	let activeUrl = $state('');
+	function setupTitle() {
+		title = window.api.settings.getTitle();
+	}
+
+	$effect(() => {
+		activeUrl= $page.url.pathname;
+		setupTitle();
+	});
 </script>
+
+<Navbar color="primary">
+	<NavBrand href="/">
+		<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white">{title}</span>
+	</NavBrand>
+	<NavHamburger />
+	<NavUl {activeUrl}>
+		<NavLi href="/"><HomeOutline /></NavLi>
+		<NavLi href="/settings"><CogOutline /></NavLi>
+	</NavUl>
+	<span class="inline-flex"> Température</span>
+
+	<Button href="/login" class="hidden sm:block">Login</Button>
+</Navbar>
+<p>{activeUrl}</p>
 
 {@render children()}
--- svelte-app/src/routes/+page.svelte
+++ svelte-app/src/routes/+page.svelte
@@ -1,2 +1,5 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import { Heading } from 'flowbite-svelte';
+</script>
+
+<Heading>HOME</Heading>

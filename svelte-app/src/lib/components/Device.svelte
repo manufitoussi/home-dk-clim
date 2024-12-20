4df3820ee7221c15ef5b6ec7380152bd50878eb8
Emmanuel FITOUSSI <manu.fitoussi@gmail.com>
2024-12-20 22:01:31 +0100

    Réduit la hauteur du composant Device pour améliorer l'affichage

--- svelte-app/src/lib/components/Device.svelte
+++ svelte-app/src/lib/components/Device.svelte
@@ -11,7 +11,7 @@
   const { device }: Props = $props();
 </script>
 
-<div in:fade|global={{ delay: 250 }} class="flex h-72 w-96 flex-col rounded border">
+<div in:fade|global={{ delay: 250 }} class="flex h-60 w-96 flex-col rounded border">
   <div class="flex w-full gap-2 rounded rounded-b-none bg-gray-600 p-3 text-white">
     <DeviceIcon icon={device.icon} />
     {device.name}

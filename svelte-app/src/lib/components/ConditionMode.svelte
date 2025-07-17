c2a3eeb04528026fba2bed11a68565c57b4a7086
Emmanuel FITOUSSI <manu.fitoussi@gmail.com>
2025-07-18 00:21:08 +0200

    Ajoute le composant ConditionMode : intègre la gestion des modes 'cold' et 'hot' avec des icônes correspondantes et met à jour le layout pour utiliser ce composant.

---
+++ svelte-app/src/lib/components/ConditionMode.svelte
@@ -0,0 +1,25 @@
+<script module lang="ts">
+  export const MODES = {
+    'cold': ThermometerSnowflake,
+    'hot': ThermometerSun,
+  };
+</script>
+
+<script lang="ts">
+  import {
+    ThermometerSun,
+    ThermometerSnowflake,
+    Thermometer 
+  } from 'lucide-svelte';
+
+  interface ConditionModeProps {
+    mode: 'cold' | 'hot';
+    size?: string | number | undefined;
+    className?: string; 
+  }
+
+  const { mode, size, className, ...others} : ConditionModeProps = $props();
+  let TheIcon = $derived(MODES[mode as keyof typeof MODES] || Thermometer);
+</script>
+
+<TheIcon {size} class={className} {...others} />
--- svelte-app/src/routes/+layout.svelte
+++ svelte-app/src/routes/+layout.svelte
@@ -2,16 +2,26 @@
   import '../app.css';
 
   import { page } from '$app/state';
+  import ConditionMode from '$lib/components/ConditionMode.svelte';
   import NavButton from '$lib/components/NavButton.svelte';
-  import { ArrowBigLeft, RefreshCcw, Settings2, Thermometer } from 'lucide-svelte';
+  import { Button, Dropdown, DropdownItem } from 'flowbite-svelte';
+  import { ChevronDownOutline } from 'flowbite-svelte-icons';
+  import { ArrowBigLeft, RefreshCcw, Settings2 } from 'lucide-svelte';
   import { fade, fly } from 'svelte/transition';
-
+  
   let { children, data } = $props();
-
+  
   let activeUrl = $derived(page.url.pathname);
+  let dropdownIconOpen = $state(false);
+  let currentMode = $state<'cold' | 'hot'>('cold');
+  let outsideConditionMode = $derived<'cold' | 'hot'>(currentMode === 'cold' ? 'hot' : 'cold');
 
   const { settings } = data;
 </script>
+
+{#snippet conditionModeIconComponent(mode: 'cold' | 'hot')}
+  <ConditionMode {mode} className="text-{mode === 'cold' ? 'blue' : 'red'}-500" />
+{/snippet}
 
 <div in:fade|global class="flex h-screen w-full flex-1 flex-col overflow-hidden bg-white">
   <div class="flex items-center gap-1 bg-[#216dcb] p-2 text-white">
@@ -22,15 +32,38 @@
       </div>
     {:else}
       <div in:fly={{ x: '-100%' }} class="flex flex-1 items-center gap-1">
+        <Button outline size="xs" class="border-none" color="light">
+          {@render conditionModeIconComponent(currentMode)}
+          <ChevronDownOutline class="ms-2 h-6 w-6" />
+        </Button>
+        <Dropdown bind:open={dropdownIconOpen}>
+          {#each ['cold', 'hot'] as mode}
+            <DropdownItem
+              class="flex w-[7em] items-center gap-2"
+              on:click={() => {
+                // Handle mode selection
+                dropdownIconOpen = false; // Close the dropdown after selection
+                // Additional logic for mode selection can be added here
+                currentMode = mode as 'cold' | 'hot'; // Update the current mode
+                console.log(`Current mode updated to: ${currentMode}`); // Log the updated mode
+              }}
+            >
+              {@render conditionModeIconComponent(mode as 'cold' | 'hot')}
+            </DropdownItem>
+          {/each}
+        </Dropdown>
+
+
         <span class="self-center whitespace-nowrap text-2xl font-semibold">
+
           Climatisation {settings.title}</span
         >
 
         <div class="flex-1"></div>
 
         <div class="flex items-center gap-1 text-xl">
-          Extérieur <div class="mx-3 flex items-center gap-1 text-3xl font-semibold">
-            <Thermometer size="0.9em" /> 9°C
+          <ConditionMode mode={outsideConditionMode} /> <div class="mx-3 flex items-center gap-1 text-2xl font-semibold">
+            9°C
           </div>
         </div>
         <NavButton><RefreshCcw /></NavButton>
